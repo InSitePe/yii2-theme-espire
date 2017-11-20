5e758d8397e7e332dbabaf3dc05be06c2f5032ad
Jose Nolberto Vilchez Moreno <jose.vilchez.m@upch.pe>
2017-11-20 16:01:42 -0500

    app.js core

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,33 +1,118 @@
-var Application = Application || {};
+// Main JavaScripts
 
-Application.system = function (exports, $) {
+(function ($) {
+	'use strict';
+	
+	// Side Nav
+	function sideNav() {
+		$('.side-nav .side-nav-menu li a').on('click', function(e) {
+			if ($(this).parent().hasClass("open")) {
 
-    'use strict';
+				$(this).parent().children('.dropdown-menu').slideUp(200, function() {
+					$(this).parent().removeClass("open");
+				});
 
-    exports.init = function () {
+			} else {
+				$(this).parent().parent().children('li.open').children('.dropdown-menu').slideUp(200);
+				$(this).parent().parent().children('li.open').children('a').removeClass('open');
+				$(this).parent().parent().children('li.open').removeClass("open");
+				$(this).parent().children('.dropdown-menu').slideDown(200, function() {
+					$(this).parent().addClass("open");
+				});
+			}
+		});
+	}	
+	
+	// Side Nav Toogle
+	function sideNavToggle() {
+		$('.side-nav-toggle').on('click', function(e) {
+	        $('.app').toggleClass("is-collapsed");
+	        e.preventDefault();
+	    });
+	}	
 
-        $('[data-toggle="tooltip"]').tooltip();
+	// Side Panel Toogle
+	function sidePanelToggle() {
+		$('.side-panel-toggle').on('click', function(e) {
+	        $('.side-panel').toggleClass("side-panel-open");
+	        e.preventDefault();
+	    });
+	}		
 
-        $('nav .open-sidenav').on('click.system.module', _click_open_sidenav);
+	// Todo Toggle
+	function todoToggle() {
+		$('.todo-toggle').on('click', function(e) {
+	        $('.todo-wrapper').toggleClass("open");
+	        e.preventDefault();
+	    });
+	}	
 
-    };
+	// Search Toggle 
+	function searchToggle() {
+		$('.search-toggle').on('click', function(e) {
+			$('.search-box, .search-input').toggleClass("active");
+			$('.search-input input').focus();
+			e.preventDefault();
+		});
+	}	
+
+	// Advance Search
+	function advanceSearch() {
+		$('.search-input input').on('keyup',function(){
+			if($(this).val().length > 0) {
+				$('.advanced-search').addClass("active");
+			} 
+			else {
+		       $('.advanced-search').removeClass("active");
+		    }
+		    $('.serach-text-bind').html($(this).val());
+		})	
+	} 
 
 
-    var _click_open_sidenav = function (e) {
-        var $html = $('html');
-        if (!$html.hasClass('sidenav-opened')) {
-            $html.addClass('sidenav-opened');
-        } else {
-            $html.removeClass('sidenav-opened');
-        }
-        e.preventDefault();
-    };
+	// Perfect Scroll Bar
+	function perfectSB() {
+		$('.scrollable').perfectScrollbar();
+	}
 
-    return exports;
+	//Card Portlet Refresh
+	function cardPortletCtrl() {
 
-}(Application.system = (Application.system || {}), jQuery);
+		//card refresh
+		$('[data-toggle=card-refresh]').on('click',function(e) {
+			var cardRefreshSelector = $(this).parents('.card');
+		    cardRefreshSelector.addClass('card-refresh');
+		    window.setTimeout(function () {
+		        cardRefreshSelector.removeClass('card-refresh');
+		    }, 2000);
+		    e.preventDefault();
+		    e.stopPropagation();
+		});
+
+		//card delete
+		$('[data-toggle=card-delete]').on('click',function(e) {
+			var cardDeleteSelector = $(this).parents('.card');
+		    cardDeleteSelector.addClass('animated zoomOut');
+		    cardDeleteSelector.bind('animationend webkitAnimationEnd oAnimationEnd MSAnimationEnd', function () {
+		        cardDeleteSelector.remove();
+		    });
+		    e.preventDefault();
+		    e.stopPropagation();
+		});
+	}
 
 
-$(function () {
-    Application.system.init();
-});
+
+
+	function init() {
+	    sideNav();
+	    sideNavToggle();
+	    sidePanelToggle();
+	    todoToggle();
+	    searchToggle();
+	    advanceSearch();
+	    perfectSB();
+	    cardPortletCtrl();
+	}
+	init();
+})(jQuery);
